20a276ad0849d6ca34164e45a04a5134bf985dfd
daniels cai <danxcai@gmail.com>
2012-08-24 21:54:39 +0800

    myring, last to frist message

--- erl/myring.erl
+++ erl/myring.erl
@@ -2,11 +2,15 @@
 -export([start/1,start_proc/2]).
 
 start(Num)->
-  start_proc(Num,self()).
+  FristLoop=spawn(myring,start_proc,[Num,self()]),
+  receive last -> io:format("~w~w~n",[last,FristLoop]) end,
+  FristLoop!ok.
 
-start_proc(0,Pid)->
-  Pid!ok;
-start_proc(Num,Pid)->
-  NPid=spawn(myring,start_proc,[Num-1,Pid]),
-  NPid!ok,
-  receive ok->ok end.
+start_proc(0,Start)->
+  Start!last;
+start_proc(Num,Start)->
+  NPid=spawn(myring,start_proc,[Num-1,Start]),
+  receive 
+    ok-> io:format("~w~w~n",[Num,NPid])
+  end,
+  NPid!ok.
