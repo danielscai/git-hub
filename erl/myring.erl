b7614bc56096db287487e4abc7dc5d7f28bcfc9f
daniels cai <danxcai@gmail.com>
2012-08-28 22:18:43 +0800

    myring doesnot work

--- erl/myring.erl
+++ erl/myring.erl
@@ -1,5 +1,6 @@
 -module(myring).
 -export([start/1,start_proc/2]).
+-export([sleep/2]).
 
 start(Num)->
   FristLoop=spawn(myring,start_proc,[Num,self()]),
@@ -9,8 +10,23 @@ start(Num)->
 start_proc(0,Start)->
   Start!last;
 start_proc(Num,Start)->
+%%  io:format("~w -- ",[Num rem 1000]), 
+%%    0->io:format("~w -- ",[Num])
+%%    0->ok
+%%    0->sleep(1000)
+%%  end,
+%%  sleep(Num,1000),
   NPid=spawn(myring,start_proc,[Num-1,Start]),
   receive 
     ok-> io:format("number:~w - pid:~w ~n",[Num,NPid])
+%%      ok->ok
   end,
   NPid!ok.
+
+sleep(Num,T)->
+  case Num rem 100 of
+    0-> 
+      receive
+      after T -> ok
+      end
+  end.
