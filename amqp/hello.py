a37df15804174aae3365bdb32406de74ea894c2b
daniels cai <danxcai@gmail.com>
2012-08-30 07:01:29 +0800

    for hello in the loop

--- amqp/hello.py
+++ amqp/hello.py
@@ -5,7 +5,8 @@ import pika
 connection=pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel=connection.channel()
 channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='',
+for i in range(0,100000):
+  channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='hello world!')
 
